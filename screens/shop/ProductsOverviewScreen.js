c8834a56b4bb46c31719b5f171e9b7465d783736
Brad Korman <brad.korman@remarque-creative.com>
2020-10-21 18:30:11 -0400

    added actions and reducers and dummmy data

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,17 @@
+//import libraries
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+// create a component
+const ProductsOverviewScreen = (props) => {
+    return (
+        <FlatList />
+    )
+};
+
+// define your styles
+const styles = StyleSheet.create({
+});
+
+//make this component available to the app
+export default ProductsOverviewScreen;
