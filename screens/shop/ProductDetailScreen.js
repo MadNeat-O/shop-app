4b751781a5db50a928651cca450949fbcce48ca6
Brad Korman <brad.korman@remarque-creative.com>
2020-10-22 22:56:22 -0400

    added basic styling to ProductDetailScreen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,7 +1,8 @@
 //import libraries
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, Button } from 'react-native';
 import { useSelector } from 'react-redux';
+import Colors from '../../constants/Colors';
 
 // create a component
 const ProductDetailScreen = (props) => {
@@ -10,16 +11,34 @@ const ProductDetailScreen = (props) => {
         state.products.availableProducts.find(prod => prod.id === productId)
     )
     return (
-        <View>
-            <Text>
-                {selectedProduct.title}
-            </Text>
-        </View>
+        <ScrollView>
+            <Image 
+                style={styles.image}
+                source={{uri: selectedProduct.imageUrl}} 
+            />
+            <Button 
+                title='Add to Cart'
+                color={Colors.primary} 
+                onPress={() => {}} 
+            />
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 };
 
 // define your styles
 const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    }
 });
 
 //make this component available to the app
